12ae47f0d8ec02dde8e8c89de4e5d20ebbb91cd4
Елена Аксенова <kittrisprior@gmail.com>
2025-02-19 20:02:56 +0500

    test methods

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from pydantic import BaseModel, ConfigDict
 
 app = FastAPI()
-@app.get("/")
+class STaskAdd(BaseModel):
+   name: str
+   description: str | None = None
 
+class STask(STaskAdd):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+@app.post("/")
+async def add_task(task: STaskAdd = Depends()):
+   return {"data": task}
+
+
+@app.get("/")
 async def home():
     return {"data": "Hi there!"}
