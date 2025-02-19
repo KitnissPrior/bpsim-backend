535385eb8c8e8982c725b64e28595ab8052d40ac
Елена Аксенова <kittrisprior@gmail.com>
2025-02-19 23:49:23 +0500

    requests settings

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from fastapi import FastAPI, Depends
-
 from pydantic import BaseModel, ConfigDict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Либо конкретные URL-адреса
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-class NodeAdd(BaseModel):
-   name: str
-   description: str | None = None
+class NodeCreation(BaseModel):
+    name: str
+    description: str | None = None
 
-class Node(NodeAdd):
+class Node(NodeCreation):
     id: int
     model_config = ConfigDict(from_attributes=True)
 
-@app.post("/")
-async def add_node(node: NodeAdd = Depends()):
-   return {"data": node}
-
+@app.post("/node/")
+async def add_node(node: NodeCreation):
+    """
+    Добавить узел в модель
+    """
+    return {"data": node}
 
 @app.get("/")
 async def home():
+    """ Home endpoint """
     return {"data": "Это бэкенд BPsim Web!"}
