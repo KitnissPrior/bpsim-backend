9a8d77c539abf0bf64a60b3282c89685384f6dc3
Елена Аксенова <kittrisprior@gmail.com>
2025-02-19 20:33:06 +0500

    start push

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from fastapi import FastAPI, Depends
 from pydantic import BaseModel, ConfigDict
 
 app = FastAPI()
-class STaskAdd(BaseModel):
+
+
+class NodeAdd(BaseModel):
    name: str
    description: str | None = None
 
-class STask(STaskAdd):
+class Node(NodeAdd):
     id: int
     model_config = ConfigDict(from_attributes=True)
 
 @app.post("/")
-async def add_task(task: STaskAdd = Depends()):
-   return {"data": task}
+async def add_node(node: NodeAdd = Depends()):
+   return {"data": node}
 
 
 @app.get("/")
 async def home():
-    return {"data": "Hi there!"}
+    return {"data": "Это бэкенд BPsim Web!"}
