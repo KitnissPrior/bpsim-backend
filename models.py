2badb1023a6e2ac5c3adefdaa8f1d65f37a58851
Елена Аксенова <kittrisprior@gmail.com>
2025-03-01 15:50:42 +0500

    classes Relation, SubjectArea, Model added

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, String, Integer
+from sqlalchemy import Column, Float, String, Integer, ForeignKey
 from database import Base
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
@@ -19,3 +19,29 @@ class Node(Base):
     description = Column(String)
     posX = Column(Float)
     posY = Column(Float)
+
+class Relation(Base):
+    __tablename__ = "relation"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    source_id = Column(Integer, ForeignKey('nodes.id'))
+    target_id = Column(Integer, ForeignKey('nodes.id'))
+
+class SubjectArea(Base):
+    __tablename__ = "subject_areas"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String)
+    description = Column(String)
+
+class Model(Base):
+    __tablename__ = "models"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String)
+    description = Column(String)
+    #FK
+    sub_area_id = Column(Integer, ForeignKey('subject_areas.id'))
+    # FK
+    model_id = Column(Integer, ForeignKey('models.id'))
+
