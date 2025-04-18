6961ce91e9ced2063b6362611033a9d6b4bed721
Елена Аксенова <kittrisprior@gmail.com>
2025-04-18 22:37:19 +0500

    resource limits added to simulation

--- simulation/sim.py
+++ simulation/sim.py
@@ -8,7 +8,8 @@ def get_events_list(nodes: [], relations: []) -> list:
     events_list = []
     #выполняем топологическую сортировку узлов по связям между ними
     sorted_node_ids = get_sorted_node_ids(relations)
-    # словарь для быстрого доступа к узлам по ID
+
+    # создаем словарь для быстрого доступа к узлам по ID
     node_dict = {node.id: node for node in nodes}
 
     # формируем новый список в нужном порядке
@@ -22,6 +23,13 @@ report = []
 simulation_res_table = []
 current_res_values = {}
 
+def set_resource_value_in_limits(new_val, min_val, max_val):
+    if new_val < min_val:
+        return min_val
+    if new_val > max_val:
+        return max_val
+    return new_val
+
 def change_resources(node_resources: [NodeRes], time: float):
     global current_res_values, simulation_res_table
     for res in node_resources:
@@ -34,19 +42,33 @@ def change_resources(node_resources: [NodeRes], time: float):
 
         report.append(f"Выполняется операция {math_operation} над ресурсом {sys_name} '{current_res['name']}'...")
 
-        report.append(f"Текущее значение ресурса {current_res['name']}: {current_res['value']}")
+        report.append(f"Текущее значение ресурса {current_res['name']}: {current_res['current_value']}")
         coefficient = float(math_operation[1:])
 
+        min_value = current_res['min_value']
+        max_value = current_res['max_value']
+
         if math_operation[0] == "+" or math_operation[0] == "-":
-            current_res['value'] = current_res_values[other_res_sys_name]['value'] + float(math_operation)
+            current_res['current_value'] = set_resource_value_in_limits(
+                current_res_values[other_res_sys_name]['current_value'] + float(math_operation),
+                min_value,
+                max_value)
+
         elif math_operation[0] == "/":
-            current_res['value'] = current_res_values[other_res_sys_name]['value'] / coefficient
+            current_res['current_value'] = set_resource_value_in_limits(
+                current_res_values[other_res_sys_name]['current_value'] / coefficient,
+                min_value,
+                max_value)
+
         elif math_operation[0] == "*":
-            current_res['value'] = current_res_values[other_res_sys_name]['value'] * coefficient
+            current_res['current_value'] = set_resource_value_in_limits(
+                current_res_values[other_res_sys_name]['current_value'] * coefficient,
+                min_value, max_value)
 
         simulation_res_table.append(SimulationRes(id=current_res['id'], sys_name=sys_name, time=time,
-                                                      name=current_res['name'], value=current_res['value']))
-        report.append(f"Новое значение ресурса {current_res['name']}: {current_res['value']}")
+                                                      name=current_res['name'],
+                                                  value=current_res['current_value']))
+        report.append(f"Новое значение ресурса {current_res['name']}: {current_res['current_value']}")
 
 def change_resources_out(node_resources_out: [NodeRes], env, duration, name):
     global report
@@ -76,8 +98,12 @@ def get_report(events: [], time_limit: int, sub_area_resources: [Resource]):
     simulation_res_table.clear()
     #формируем словарь текущих значений ресурсов ПО для хранения информации об изменениях на входе/выходе;
     #ключ - системное имя ресурса
-    res_values_list = [{'sys_name': res.sys_name, 'id': res.id,
-                        'name': res.name, 'value': res.current_value}
+    res_values_list = [{'sys_name': res.sys_name,
+                        'id': res.id,
+                        'name': res.name,
+                        'current_value': res.current_value,
+                        'min_value': res.min_value,
+                        'max_value': res.max_value}
                        for res in sub_area_resources]
     current_res_values = {res['sys_name']: res for res in res_values_list}
 
